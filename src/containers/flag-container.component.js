aceff8b77ceafe98ebb26b9f972a89d5294e4c86
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-02 20:19:59 +0200

    add delete country

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries, searchCountries } from '../actions/actions-countries';
+import { getCountries, searchCountries, deleteCountry } from '../actions/actions-countries';
 
 class CountryFlagContainer extends Component {
   constructor(props) {
@@ -10,11 +10,15 @@ class CountryFlagContainer extends Component {
 
   componentDidMount() {
     this.props.dispatch(getCountries());
-    this.props.dispatch(searchCountries());
+    this.props.dispatch(searchCountries(''));
   }
 
   search(event) {
     this.props.dispatch(searchCountries(event.target.value));
+  }
+
+  deleteCountry(id) {
+    this.props.dispatch(deleteCountry(id));
   }
 
   render() {
@@ -23,13 +27,13 @@ class CountryFlagContainer extends Component {
         <div className="search text-center">
           <input type="text" onChange={this.search.bind(this)} />
         </div>
-        <CountryFlagList countries={this.props.visibleCountries} />
+        <CountryFlagList countries={this.props.visibleCountries} deleteCountry={this.deleteCountry.bind(this)} />
       </div>
     )
   }
 }
 
-const mapStateToProps = function(store) {
+const mapStateToProps = function (store) {
   return {
     countries: store.countriesReducer.countries,
     visibleCountries: store.countriesReducer.visibleCountries

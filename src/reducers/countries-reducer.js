fe34d5444995cf934bc3c6e048c54d565da445a2
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-02 19:20:11 +0200

    add get country case

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -8,13 +8,17 @@ import {
 import countriesData from '../data/countries.json';
 
 const initialState = {
-  countries: countriesData
+  countries: countriesData,
+  selectedCountry: {}
 }
 
 const countriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_COUNTRIES:
       return Object.assign({}, state, {countries: state.countries});
+    case GET_COUNTRY:
+      const selectedCountry = state.countries.find(country => country.id === parseInt(action.id));
+      return Object.assign({}, state, { selectedCountry });
     default:
       console.log(`default`);
   }

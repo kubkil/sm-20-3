c249713ea780c2f5897aeac344921f321248815a
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-02 17:53:14 +0200

    create flag component

---
+++ src/presentational/flag.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// czy można zastąpić
+// const CountryFlag = country => (
+//   <div className="country-logo-wrapper">
+//     <img className="country-logo" src={country.imageUrl} alt="country photo" />
+//   </div>
+// );
+const CountryFlag = (props) => (
+  <div className="country-logo-wrapper">
+    <img className="country-logo" src={props.country.imageUrl} alt="country photo" />
+  </div>
+);
+
+export default CountryFlag;
